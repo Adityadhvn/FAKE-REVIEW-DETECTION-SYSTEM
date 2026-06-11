6a092ea2d5dc53f1dfadfde05c772fde35f94c2e
Adityadhvn <adityadhawan46@gmail.com>
2026-06-12 01:28:23 +0530

    Added Runtime.txt to counter Streamlit Deployment Issues

--- test_distilbert.py
+++
@@ -1,8 +0,0 @@
-from src.predict_logistic import predict_review
-
-pred, conf = predict_review(
-    "This product is absolutely amazing and exceeded my expectations."
-)
-
-print("Prediction:", pred)
-print("Confidence:", conf)
